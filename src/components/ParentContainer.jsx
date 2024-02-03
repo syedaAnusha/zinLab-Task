54e55b5178c2c031cd56386af17914bebad7c113
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-02-03 18:15:30 +0500

    update Aboutus

--- src/components/ParentContainer.jsx
+++ src/components/ParentContainer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "@/styles/parentContainer.module.css";
 import Header from "./Header";
-
+import Aboutus from "./Aboutus";
 const ParentContainer = () => {
   return (
     <div className={styles.parent__container}>
       <Header />
+      <Aboutus />
     </div>
   );
 };
