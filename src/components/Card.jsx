b3333c804cdbe812c7bfbc906b33ce2114c825b9
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-02-16 17:04:14 +0500

    Implement bookmark functionality on click event

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { GoBookmarkFill } from "react-icons/go";
 import styles from "@/styles/card.module.css";
 
 const Card = ({ src, alt, title, description }) => {
+  const [isBookmarked, setBookmarked] = useState(false);
+
+  function handler() {
+    setBookmarked(!isBookmarked);
+  }
+
   return (
     <>
       <div className={styles.main}>
-        <span className={styles.bookmark}>
-          <GoBookmarkFill className={styles.bookmark} size={20} />
+        <span className={styles.span}>
+          <GoBookmarkFill
+            className={styles.bookmark}
+            size={20}
+            fill={isBookmarked ? "#333" : "lightslategrey"}
+            onClick={handler}
+          />
         </span>
         <Image
           src={src}
